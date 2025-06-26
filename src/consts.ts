61826cb8a072861efdb9394c8c866842c65533c6
さりえり <nakakou201250@gmail.com>
2025-06-26 09:13:19 +0000

    [feat] #1 Astro（blog template）を設定する
    UIフレームワークに依存せず，必要最小限の構成でプロジェクトを公開できるようにするため．
    ブログなどのインプット/アウトプットも併せてポートフォリオだと考えているので，blog templateで初期化する．
    外部のブログサービスは制限があるので，自律的に公開できるよう自作ブログを構築する．

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Portfolio | salieri256';
+export const SITE_DESCRIPTION = 'Welcome to salieri256\'s Portfolio!';
